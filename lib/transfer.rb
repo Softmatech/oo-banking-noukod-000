419c970ea9f908dcaa80f3ddcf62f750ab15fd3e
Andy Feidje Joseph <ingandy75@gmail.com>
2019-05-02 18:55:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,16 @@
 class Transfer
-  # your code here
+attr_reader :name, :sender, :amount
+attr_accessor :sender, :receiver, :status, :bank_account
+
+  def initialize(sender,receiver,status)
+      @sender = sender
+      @receiver = receiver
+      @status = "pending"
+      @amount = 50
+  end
+
+  def valid?
+      @receiver.valid? ? true :false
+  end
+
 end
