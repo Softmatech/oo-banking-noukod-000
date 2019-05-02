b109efa4cdb299657dde9749776496e908011d8c
Andy Feidje Joseph <ingandy75@gmail.com>
2019-05-02 19:46:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,4 +26,12 @@ attr_accessor :sender, :receiver, :status, :amount
       end
   end
 
+  def reverse_transfer
+    if @status = "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
+
 end
