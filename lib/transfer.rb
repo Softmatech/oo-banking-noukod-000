d11fe833063ae44da9be91cfeb35e7b21a2bd59b
Andy Feidje Joseph <ingandy75@gmail.com>
2019-05-02 19:09:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ attr_accessor :sender, :receiver, :status, :bank_account
   end
 
   def execute_transaction
-      @sender.valid? ? "Ok" : "Transaction rejected. Please check your account balance."
+      @sender.valid? ? "Transaction rejected. Please check your account balance." : "Transaction rejected. Please check your account balance."
   end
 
 end
